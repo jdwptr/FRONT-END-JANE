041cb15e02142615a36eb0f84c76a9805b7d8247
jdwptr <jane.dwptrdjaja@gmail.com>
2020-11-25 15:50:10 +0700

    ok

--- DAY06/intro-to-react/src/components/header.js
+++ DAY06/intro-to-react/src/components/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './header.css'
 
 class Header extends React.Component {
     render () {
         return (
-        <ul>
+        <ul className='list'>
             <li>santa</li>
             <li>claus</li>
             <li>elves</li>
